16f74a2311da794d72b18038a2e411ded34cdcf9
Javier Epifanio López <you@example.com>
2020-10-06 18:58:11 +0200

    Ejercicio 2 de secuenciales (Rectángulo)

---
+++ Ejercicios secuenciales/ej2.py
@@ -0,0 +1,14 @@
+# Ejercicio 1 de secuenciales: Calcular el perí­metro y área de un rectángulo dada su base y su altura.
+# Autor: Javier Epifanio López.
+# Fecha: 05/10/2020
+
+# Pedimos los datos (base y altura)
+base = int(input("Base del rectángulo (en cm): "))
+altura = int(input("Altura del rectángulo (en cm): "))
+
+# Calculamos el perímetro y el área
+perimetro = 2 * (base + altura)
+area = base * altura
+
+# Mostramos por pantalla el resultado del perímetro y del área
+print("El perímetro del rectángulo:",perimetro, "y el área del rectángulo:",area)
