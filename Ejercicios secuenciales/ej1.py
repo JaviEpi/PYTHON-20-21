77060873a66f42e36f458224dcba97e9e9668622
Javier Epifanio <javi.epi10@gmail.com>
2020-10-12 18:53:33 +0200

    cambio de nombre

--- Ejercicios secuenciales/ej1.py
+++
@@ -1,9 +0,0 @@
-# Ejercicio 1 de secuenciales: Escribir un programa que pregunte al usuario su nombre, y luego lo salude.
-# Autor: Javier Epifanio López
-# Fecha: 05/10/2020
-
-# Pedimos el nombre del usuario
-nombre = input("¿Cómo te llamas?\n")
-
-# Mostramos por pantalla el saludo al usuario
-print("Hola " + nombre)
